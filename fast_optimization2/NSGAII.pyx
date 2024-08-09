5c99fc8a798a44eaeeb2cdaf24e521f5bc371e94
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-08-09 11:01:40 +0200

    starting

--- fast_optimization2/NSGAII.pyx
+++ fast_optimization2/NSGAII.pyx
@@ -78,13 +78,13 @@ cpdef np.ndarray[double, ndim=1] calculate_crowding_distance(np.ndarray[double, 
         print("objectives[front, m]:", objectives[front, m])
         print("Indices acessados:", sorted_indices[0], sorted_indices[len(sorted_indices)-1])
  
-        distance[sorted_indices[-1]] = 1e308 # np.inf
+        distance[sorted_indices[len(sorted_indices)-1]] = 1e308 # np.inf
         print("Check crowding 2")
 
         for i in range(1, num_individuals - 1):
             next_value = objectives[front[sorted_indices[i + 1]], m]
             prev_value = objectives[front[sorted_indices[i - 1]], m]
-            norm = objectives[front[sorted_indices[-1]], m] - objectives[front[sorted_indices[0]], m]
+            norm = objectives[front[sorted_indices[len(sorted_indices)-1]], m] - objectives[front[sorted_indices[0]], m]
             if norm != 0:
                 distance[sorted_indices[i]] += (next_value - prev_value) / norm
         print("Check crowding 3")
